d6eadd86f54cf135fffa93c2e62b2f9247ef3aea
diogenes <68737524+diogenes-de-sinope@users.noreply.github.com>
2020-07-24 12:17:27 -0400

    Update test.py

--- main/test.py
+++ main/test.py
@@ -1,9 +1,9 @@
-import serial
+iimport serial
 
 port = '/dev/ttyUSB0'
 
 uart = serial.Serial(port, baudrate = 115_200, bytesize = 8, parity = 'N', stopbits = 1, timeout = 0)
 
 while True:
-    If uart.in_waiting > 0:
+    if uart.in_waiting > 0:
         print(uart.read(uart.in_waiting))
