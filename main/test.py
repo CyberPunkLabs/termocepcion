33d558793b6d4803ce5e3bdbd1257f972ef05e97
diogenes <68737524+diogenes-de-sinope@users.noreply.github.com>
2020-07-24 12:42:52 -0400

    Update test.py

--- main/test.py
+++ main/test.py
@@ -1,9 +1,20 @@
-iimport serial
+
+import atexit
+import time
+import numpy
+#import matplotlib.pyplot as pyplot
+
+from Chamber import *
+from Pid import *
+from SetPoint import *
 
 port = '/dev/ttyUSB0'
 
-uart = serial.Serial(port, baudrate = 115_200, bytesize = 8, parity = 'N', stopbits = 1, timeout = 0)
+
+chamber = Chamber(port)
 
 while True:
-    if uart.in_waiting > 0:
-        print(uart.read(uart.in_waiting))
+    temp = chamber.read()
+
+    if len(temp) > 0:
+        print(temp)
